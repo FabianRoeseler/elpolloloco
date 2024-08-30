d1e3f940315805b42184d77ffaba0ab607c736bb
crimme <kueppers.fabian@web.de>
2024-08-30 13:36:56 +0200

    update

--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
 let canvas;
-let character = new Character();
+let ctx;
+let world;
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  console.log("My character is", character);
+  world = new World(canvas);
+
+  console.log("My character is", world.character);
 }
