e466dbf1a7133540aefe851cbbfb0e0419d01ba2
crimme <kueppers.fabian@web.de>
2024-09-02 11:16:55 +0200

    bg and height fixed

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,9 +1,9 @@
 class BackgroundObject extends MovableObject {
   width = 720;
-  height = 400;
+  height = 480;
   constructor(imagePath, x) {
     super().loadImage(imagePath);
     this.x = x;
-    this.y = 480 - this.height; // 480 - 80
+    this.y = 480 - this.height;
   }
 }
