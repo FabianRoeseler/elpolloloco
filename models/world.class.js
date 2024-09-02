bd9b516d12cb6e5db541c040210050e958d3295f
crimme <kueppers.fabian@web.de>
2024-09-02 10:41:03 +0200

    bg switch

--- models/world.class.js
+++ models/world.class.js
@@ -3,7 +3,7 @@ class World {
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Cloud()];
   backgroundObjects = [
-    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0, 80),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
   ];
   canvas;
   ctx;
@@ -17,10 +17,11 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.addObjectsToMap(this.backgroundObjects);
+
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
-    this.addObjectsToMap(this.backgroundObjects);
 
     let self = this;
     requestAnimationFrame(function () {
