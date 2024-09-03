b83cb4080167ef23d243b7bac418f00c501b8bca
crimme <kueppers.fabian@web.de>
2024-09-03 12:04:41 +0200

    moving animation enemy refactored

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,15 +1,15 @@
 class Chicken extends MovableObject {
-  height = 100;
-  CHICKEN_WALK = [
+  height = 80;
+  IMAGES_WALKING = [
     "../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
     "../img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
     "../img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
   ];
   constructor() {
     super().loadImage("../img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
-    this.loadImages(this.CHICKEN_WALK);
+    this.loadImages(this.IMAGES_WALKING);
 
-    this.x = 300 + Math.random() * 500;
+    this.x = 300 + Math.random() * 2500;
     this.y = 330;
     this.speed = 0.15 + Math.random() * 0.25;
     this.animate();
@@ -17,10 +17,7 @@ class Chicken extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.CHICKEN_WALK.length;
-      let path = this.CHICKEN_WALK[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
+      this.playAnimation(this.IMAGES_WALKING);
     }, 250);
     this.moveLeft();
   }
---
+++ models/endboss.class.js
@@ -0,0 +1,27 @@
+class Endboss extends MovableObject {
+  height = 400;
+  width = 200;
+  IMAGES_WALKING = [
+    "../img/4_enemie_boss_chicken/1_walk/G1.png",
+    "../img/4_enemie_boss_chicken/1_walk/G2.png",
+    "../img/4_enemie_boss_chicken/1_walk/G3.png",
+    "../img/4_enemie_boss_chicken/1_walk/G4.png",
+  ];
+  hei;
+
+  constructor() {
+    super().loadImage("../img/4_enemie_boss_chicken/1_walk/G1.png");
+    this.loadImages(this.IMAGES_WALKING);
+    this.x = 2700;
+    this.y = 50;
+    this.speed = 0.15 + Math.random() * 0.45;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 350);
+    this.moveLeft();
+  }
+}
