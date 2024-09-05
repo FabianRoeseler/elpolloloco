22a0696adb72171ae52382a40c57abcc5618b991
crimme <kueppers.fabian@web.de>
2024-09-05 12:12:59 +0200

    hit animation added

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,6 +11,7 @@ class MovableObject {
   speedY = 0;
   acceleration = 2.5;
   energy = 100;
+  lastHit = 0;
 
   applyGravity() {
     setInterval(() => {
@@ -44,6 +45,35 @@ class MovableObject {
     }
   }
 
+  // character.isColloding(chicken);
+  isColliding(mo) {
+    return (
+      this.x + this.width > mo.x &&
+      this.y + this.height > mo.y &&
+      this.x < mo.x &&
+      this.y < mo.y + mo.height
+    );
+  }
+
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
+    }
+  }
+
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000; // change to seconds
+    return timepassed < 2;
+  }
+
+  isDead() {
+    return this.energy == 0;
+  }
+
   loadImages(arr) {
     arr.forEach((path) => {
       let img = new Image();
@@ -53,7 +83,7 @@ class MovableObject {
   }
 
   playAnimation(images) {
-    let i = this.currentImage % this.IMAGES_WALKING.length;
+    let i = this.currentImage % images.length;
     let path = images[i];
     this.img = this.imageCache[path];
     this.currentImage++;
@@ -69,26 +99,5 @@ class MovableObject {
 
   jump() {
     this.speedY = 30;
-  }
-
-  // character.isColloding(chicken);
-  isColliding(mo) {
-    return (
-      this.x + this.width > mo.x &&
-      this.y + this.height > mo.y &&
-      this.x < mo.x &&
-      this.y < mo.y + mo.height
-    );
-  }
-
-  hit() {
-    this.energy -= 5;
-    if (this.energy < 0) {
-      this.energy = 0;
-    }
-  }
-
-  isDead() {
-    return this.energy == 0;
   }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -24,7 +24,6 @@ class World {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isColliding(enemy)) {
           this.character.hit();
-          console.log(this.character.energy);
         }
       });
     }, 200);
