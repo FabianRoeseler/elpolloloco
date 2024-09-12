b61330e4aa4efa5848633fce23d27e78fa26aaa3
crimme <kueppers.fabian@web.de>
2024-09-12 11:26:16 +0200

    boss animations added-not functional

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -8,9 +8,35 @@ class Endboss extends MovableObject {
     "../img/4_enemie_boss_chicken/1_walk/G4.png",
   ];
 
+  IMAGES_ALERT = [
+    "../img/4_enemie_boss_chicken/2_alert/G5.png",
+    "../img/4_enemie_boss_chicken/2_alert/G6.png",
+    "../img/4_enemie_boss_chicken/2_alert/G7.png",
+    "../img/4_enemie_boss_chicken/2_alert/G8.png",
+    "../img/4_enemie_boss_chicken/2_alert/G9.png",
+    "../img/4_enemie_boss_chicken/2_alert/G10.png",
+    "../img/4_enemie_boss_chicken/2_alert/G11.png",
+    "../img/4_enemie_boss_chicken/2_alert/G12.png",
+  ];
+
+  IMAGES_HURT = [
+    "../img/4_enemie_boss_chicken/4_hurt/G21.png",
+    "../img/4_enemie_boss_chicken/4_hurt/G22.png",
+    "../img/4_enemie_boss_chicken/4_hurt/G23.png",
+  ];
+
+  IMAGES_DEAD = [
+    "../img/4_enemie_boss_chicken/5_dead/G24.png",
+    "../img/4_enemie_boss_chicken/5_dead/G25.png",
+    "../img/4_enemie_boss_chicken/5_dead/G26.png",
+  ];
+
   constructor() {
     super().loadImage("../img/4_enemie_boss_chicken/1_walk/G1.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_ALERT);
+    this.loadImages(this.IMAGES_HURT);
+    this.loadImages(this.IMAGES_DEAD);
     this.x = 2700;
     this.y = 50;
     this.speed = 0.15 + Math.random() * 0.45;
@@ -18,12 +44,13 @@ class Endboss extends MovableObject {
   }
 
   animate() {
+    let i = 0;
     setInterval(() => {
       this.moveLeft();
     }, 1000 / 60);
 
     setInterval(() => {
       this.playAnimation(this.IMAGES_WALKING);
-    }, 350);
+    }, 300);
   }
 }
