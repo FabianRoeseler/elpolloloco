eaeba4891b1989b8bfa3f825fb917d8c59222fea
crimme <kueppers.fabian@web.de>
2024-09-12 10:35:44 +0200

    minor changes

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -3,16 +3,16 @@ class ThrowableObject extends MovableObject {
     super().loadImage("img/6_salsa_bottle/2_salsa_bottle_on_ground.png");
     this.x = x;
     this.y = y;
-    this.height = 80;
-    this.width = 60;
+    this.height = 70;
+    this.width = 50;
     this.throw();
   }
 
   throw() {
-    this.speedY = 20;
+    this.speedY = 15;
     this.applyGravity();
     setInterval(() => {
-      this.x += 10;
+      this.x += 15;
     }, 25);
   }
 }
